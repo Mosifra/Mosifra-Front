2e052b1aa0cf41b8fc777cfd13c88ae575736aa0
Agahnim <43116178+naguiagahnim@users.noreply.github.com>
2025-09-09 11:48:51 +0200

    Deleted all template content and add navbar skeleton

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,53 @@
 import { useLocation } from 'preact-iso';
 
 export function Header() {
-	const { url } = useLocation();
+  const { url, route } = useLocation();
 
-	return (
-		<header>
-			<nav>
-				<a href="/" class={url == '/' && 'active'}>
-					Home
-				</a>
-				<a href="/404" class={url == '/404' && 'active'}>
-					404
-				</a>
-			</nav>
-		</header>
-	);
+  const isActive = (p) => url === p;
+
+  return (
+    <nav className="sticky top-0 z-50 bg-red-500">
+      <div className="flex items-center py-4 px-8 relative">
+        <div className="flex items-center bg-yellow-500 py-4 px-8">
+          <img src="/images/logo_notext.svg" className="size-16" />
+        </div>
+        <ul className="ml-auto flex list-none p-0 m-0 gap-4">
+          <li className="relative">
+            <a
+              href="/"
+              onClick={(e) => {
+                e.preventDefault();
+                route('/');
+              }}
+              className={`flex items-center gap-6 py-4 px-8 border-2 border-indigo-600 ${
+                isActive('/')
+                  ? 'bg-green-500 text-white'
+                  : 'bg-yellow-500 text-green-500'
+              }`}
+            >
+              <span>Home</span>
+            </a>
+          </li>
+          <li className="relative">
+          <a
+            href="/404"
+            onClick={(e) => {
+              e.preventDefault();
+              route('/404');
+            }}
+            className={`flex items-center gap-6 py-4 px-8 border-2 border-indigo-600 ${
+              isActive('/404')
+                ? 'bg-green-500 text-white'
+                : 'bg-yellow-500 text-green-500'
+            }`}
+          >
+            <span>404</span>
+          </a>
+        </li>
+
+        </ul>
+      </div>
+    </nav>
+  );
 }
+
--- src/pages/_404.jsx
+++ src/pages/_404.jsx
@@ -1,8 +1,49 @@
 export function NotFound() {
 	return (
 		<section>
-			<h1>404: Not Found</h1>
-			<p>It's gone :(</p>
+			<h1>404 : Y'a rien ici mon gourmand</h1>
+			<p>1° La première est celle qui représente l’art comme ayant pour but
+      d’exciter la sensation ou le plaisir. Dans ce système, les recherches sur le
+      beau dans les arts se bornent à une analyse des sensations ou des impressions
+      qu’ils nous font éprouver. Mais elles ne peuvent conduire à rien de fixe et de
+      scientifique. La sensibilité est la région obscure et indéterminée de l’esprit. La
+      sensation, étant purement subjective et individuelle, ne fournit matière qu’à
+      des distinctions et à des classifications arbitraires et artificielles. Elle admet
+      comme causes les éléments les plus opposés. Ses formes peuvent, il est vrai,
+      correspondre à la diversité des objets : c’est ainsi que l’on distingue le
+      sentiment du droit, le sentiment moral, le sentiment du sublime, le sentiment
+      religieux. Mais, par cela même que l’objet est donné sous la forme du
+      sentiment, il n’apparaît plus dans son caractère essentiel et propre. On fait
+      précisément abstraction de l’objet lui-même et de son idée, pour ne considérer
+      que les divers états ou modifications du sujet. Toutes ces analyses minutieuses
+      des sensations et des particularités qu’elles peuvent offrir finissent par être
+      fastidieuses et dénuées d’un véritable intérêt.
+      2° A cette manière d’étudier l’art se rattachent aussi les tentatives qui ont
+      été faites pour perfectionner le goût considéré comme sens du beau, tentatives
+      qui n’ont produit également rien que de vague, d’indéterminé et de superficiel.
+      Le goût ainsi conçu ne peut pénétrer dans la nature intime et profonde des
+      objets ; car celle-ci ne se révèle pas aux sens ni même au raisonnement, mais à
+      la raison, à cette faculté de l’esprit qui seule connaît le vrai, le réel, le
+      substantiel en toutes choses. Aussi ce qu’on est convenu d’appeler le bon goût
+      n’ose s’attaquer aux grands effets de l’art ; il garde le silence quand les
+      caractères extérieurs et accessoires font place à la chose elle-même. Lorsqu’en
+      effet ce sont les grandes passions et les mouvements profonds de l’âme qui
+      sont en scène, il ne s’agit plus de tout cet étalage de distinctions minutieuses
+      et subtiles sur les particularités dont le goût se préoccupe. Celui-ci sent alors
+      le génie planer au-dessus de cette région inférieure et se retire devant sa
+      puissance.
+      Quelle est donc la part du sensible dans l’art et son véritable rôle ? Il y a
+      deux manières d’envisager les objets sensibles dans leur rapport avec notre
+      esprit. 1° Le premier est celui de la simple perception des objets par les sens.
+      L’esprit alors ne saisit que leur côté individuel, leur forme particulière et
+      concrète ; l’essence, la loi, la substance des choses lui échappe. En même
+      temps le besoin qui s’éveille en nous est celui de les approprier à notre usage,
+      de les consommer, de les détruire. L’âme, en face de ces objets, sent sa
+      dépendance ; elle ne peut les contempler d’un oeil libre et désintéressé.
+      Un autre rapport des êtres sensibles avec l’esprit est celui de la pensée
+      spéculative ou de la science. Ici l’intelligence ne se contente plus de percevoir
+      l’objet dans sa forme concrète et son individualité, elle écarte le côté
+      individuel pour en abstraire et en dégager la loi, le général, l ’essence.</p>
 		</section>
 	);
 }
