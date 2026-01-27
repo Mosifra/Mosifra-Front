2e607a62abb45d6aa659d04695c7c5c746603ac9
Agahnim <gabrielguysoulard@gmail.com>
2026-01-27 11:28:55 +0100

    refacto flake to use tmux

--- flake.nix
+++ flake.nix
@@ -1,10 +1,8 @@
 {
   description = "Flake pour dev Preact + Bun + Tailwind";
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
-
   outputs = {
     self,
     nixpkgs,
@@ -14,18 +12,28 @@
     devShells."x86_64-linux".default = pkgs.mkShell {
       buildInputs = with pkgs; [
         bun
+        tmux
       ];
-
       shellHook = ''
-        echo "🌾 Bienvenue, noble artisan Preact !"
-        echo "🌀 Bun est prêt pour vos scripts et Tailwind."
         export PATH=${pkgs.bun}/bin:$PATH
-
         if [ -f bun.lockb ]; then
-          echo "💨 Installation des dépendances avec Bun…"
+          echo "Installation des dépendances avec Bun…"
           bun install
         fi
-        neovide &
+
+        SESSION_NAME="preact-dev"
+
+        if ! tmux has-session -t $SESSION_NAME 2>/dev/null; then
+          # Créer session avec 2 fenêtres
+          tmux new-session -d -s $SESSION_NAME -n "editor"
+          tmux send-keys -t $SESSION_NAME:0 "neovide" C-m
+
+          tmux new-window -t $SESSION_NAME:1 -n "shell"
+
+          tmux select-window -t $SESSION_NAME:1
+        fi
+
+        tmux attach-session -t $SESSION_NAME
       '';
     };
   };
